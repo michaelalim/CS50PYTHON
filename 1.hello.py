54ef064bbcd3d67b1f802d6e8f30d1273f4212c7
Michael Jason Alim <89329705+michaelalim@users.noreply.github.com>
2023-05-11 19:56:11 +0800

    Add files via upload

---
+++ 1.hello.py
@@ -0,0 +1,38 @@
+#HOW TO RUN PYTHON
+#!!! USE python (space) filename (in this case hello.py)
+#example of how to run: python hello.py
+
+#---------------------------------------------------------------------------------------------------------------------
+#MAIN LESSON
+
+#part 1: introduction to printing, variables, and using +, =
+
+#input used to get data for user
+name = input("What's your name? ")
+
+#print function, basically saying hello to user using the variable "name"
+print("hello,", name)
+
+"""
+NOTE print("hello, " + name) still works like javascript but arguments part (in python, I'm not sure in javascript) can accept multiple arguments by comma,
+#also note that when entering multiple arguments, there is an auto-space between arguments, hence the "hello," part doesn't have a space after the comma. The +
+#version is called concatenation. 
+"""
+
+#----------------------------------------------------------------------------------------------------------------------
+
+#TIPS on using comments --- this applies to every other programming languages (and non-programming languages)
+#Use this for structuring to-do lists to program you want to do
+
+#FOR EXAMPLE
+
+#ask user what their name is
+#input code
+
+#print hello, (name of user) on the console
+#input code
+
+#----------------------------------------------------------------------------------------------------------------------
+
+#all functions are in docs.python.org
+#more specifically in docs.python.org/3/library/functions.html
---
+++ 3.hello3.py
@@ -0,0 +1,11 @@
+# Welp, another way of solving the "one line hello, name" problem
+# NOTE this is the most used way (maybe in python), so cancel ang concatenate?
+# Also the most elegant way
+# THIS IS A NEW FEATURE OF PYTHON
+
+# ----------------------------------------------------------------------------------------------------------------------
+# CURLY BRACES
+
+name = input("What is your name? ")
+print(f"Hello, {name}")
+
